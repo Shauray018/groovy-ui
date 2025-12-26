baa3fa14d80caf1d513f0455aa88cc289417866d
Shauray018 <shauraydhingra03@gmail.com>
2025-12-27 02:57:55 +0530

    added 2 components

---
+++ sauce/src/registry/components/lightning.ts
@@ -0,0 +1,19 @@
+import { Registry } from '../index.js';
+
+export const buttonRegistry: Registry = {
+  button: {
+    name: 'lightning background',
+    description: 'crazyyyyy background',
+    type: 'registry:ui',
+    registryDependencies: [],
+    dependencies: [], // npm packages needed
+    files: [
+      {
+        type: 'registry:ui',
+        // Points to raw GitHub URL of your component
+        path: 'src/components/ui/Lightning.tsx',
+        target: 'components/ui/Lightning.tsx', // Where to copy in user's project
+      },
+    ],
+  },
+};
---
+++ sauce/src/registry/components/silk.ts
@@ -0,0 +1,19 @@
+import { Registry } from '../index.js';
+
+export const buttonRegistry: Registry = {
+  button: {
+    name: 'silk background',
+    description: 'crazyyyyy background',
+    type: 'registry:ui',
+    registryDependencies: [],
+    dependencies: [], // npm packages needed
+    files: [
+      {
+        type: 'registry:ui',
+        // Points to raw GitHub URL of your component
+        path: 'src/components/ui/Silk.tsx',
+        target: 'components/ui/Silk.tsx', // Where to copy in user's project
+      },
+    ],
+  },
+};
